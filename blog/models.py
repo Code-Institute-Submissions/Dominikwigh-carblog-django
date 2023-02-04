a938a57817945673589c8c8af1412ad57b8f8450
Dominikwigh <dominik-00@hotmail.com>
2023-02-04 10:12:10 +0000

    add a slug to post model

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
+    slug = models.CharField(max_length=255, default='blogpost')
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
 
