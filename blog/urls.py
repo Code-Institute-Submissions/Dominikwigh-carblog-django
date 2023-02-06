5bd30627f48aa7163dee41f99f2d14378b009ff6
Dominikwigh <dominik-00@hotmail.com>
2023-02-06 14:22:45 +0000

    add a url path and title with button to delete_posts.html

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
-from .views import Home, PostDetailView, AddPost, UpdatePost
+from .views import Home, PostDetailView, AddPost, UpdatePost, DeletePost
 
+
+# Urls for all pages 
 urlpatterns = [
     # path('', views.home, name='home'),
     path('', Home.as_view(), name='home'),
     path('post/<int:pk>', PostDetailView.as_view(), name='post-detail'),
     path('add_post/', AddPost.as_view(), name='add_post'),
-    path('post/edit/<int:pk>', UpdatePost.as_view(), name='update_post')
+    path('post/edit/<int:pk>', UpdatePost.as_view(), name='update_post'),
+    path('post/<int:pk>/delete', DeletePost.as_view(), name='delete_post'),
+    
 ]
