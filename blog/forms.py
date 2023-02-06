3a4802af44d4cdd177547b8e0b74f6a636ba766f
Dominikwigh <dominik-00@hotmail.com>
2023-02-06 12:21:49 +0000

    add placeholder text to widgets

--- blog/forms.py
+++ blog/forms.py
@@ -7,7 +7,8 @@ class PostForm(forms.ModelForm):
         model = Post
         fields = ('title', 'slug', 'author', 'body')
         widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your title here!'}),
             'slug': forms.TextInput(attrs={'class': 'form-control'}),
             'author': forms.Select(attrs={'class': 'form-control'}),
-            'body': forms.Textarea(attrs={'class': 'form-control'})}
+            'body': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Enter your blog text here!'})
+            }
