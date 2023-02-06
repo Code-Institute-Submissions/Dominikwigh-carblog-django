ee68feaaf39cdc05b413ea298703c16b96d6c820
Dominikwigh <dominik-00@hotmail.com>
2023-02-06 09:45:42 +0000

    create a add post view and url

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import Home, PostDetailView
+from .views import Home, PostDetailView, AddPost
 
 urlpatterns = [
     # path('', views.home, name='home'),
     path('', Home.as_view(), name='home'),
     path('post/<int:pk>', PostDetailView.as_view(), name='post-detail'),
+    path('add_post/', AddPost.as_view(), name='add_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
 
 # Home view 
@@ -10,3 +10,8 @@ class Home(ListView):
 class PostDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+class AddPost(CreateView):
+    model = Post
+    template_name = 'add_posts.html'
+    fields = '__all__'
