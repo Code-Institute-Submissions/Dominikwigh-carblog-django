cf643a8fb58506ff372ba57cf92d32d838bed8e8
Dominikwigh <dominik-00@hotmail.com>
2023-02-06 12:19:28 +0000

    create a postform class and style it with bootstrap

--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
+from .forms import PostForm
 
 # Home view 
 class Home(ListView):
     model = Post
     template_name = 'home.html'
 
+
 class PostDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
 
+
 class AddPost(CreateView):
     model = Post
+    form_class = PostForm
     template_name = 'add_posts.html'
-    fields = '__all__'
