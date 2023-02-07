743119817d6528430f73d8649a5be3d982c47ec4
Dominikwigh <dominik-00@hotmail.com>
2023-02-07 11:44:21 +0000

    create userregisterview and connect up urls

--- carblog/settings.py
+++ carblog/settings.py
@@ -132,6 +132,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
