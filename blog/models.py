dbe2f09b7dfb91784774246e54e517c380ce6255
Dominikwigh <dominik-00@hotmail.com>
2023-02-08 10:56:50 +0000

    add a category class to models.py

--- blog/models.py
+++ blog/models.py
@@ -10,9 +10,22 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
+    category = models.CharField(max_length=255, default='car')
 
     def __str__(self):
         return self.title + str(self.author)
 
     def get_absolute_url(self):
         return reverse('home')
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title + str(self.author)
+
+    def get_absolute_url(self):
+        return reverse('home')
+
+    
+
