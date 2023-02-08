9210930a06cb13925587f8faa5c9319f1f65e91b
Dominikwigh <dominik-00@hotmail.com>
2023-02-08 14:09:45 +0000

    add a dropdown menu to navbar from bootstrap

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import Home, PostDetailView, AddPost, UpdatePost, DeletePost, AddCategory
+from .views import Home, PostDetailView, AddPost, UpdatePost, DeletePost
 
 
 # Urls for all pages 
@@ -10,6 +10,5 @@ urlpatterns = [
     path('add_post/', AddPost.as_view(), name='add_post'),
     path('post/edit/<int:pk>', UpdatePost.as_view(), name='update_post'),
     path('post/<int:pk>/delete', DeletePost.as_view(), name='delete_post'),
-    path('category/<str:cat>/', AddCategory, name='category'),
     
 ]
