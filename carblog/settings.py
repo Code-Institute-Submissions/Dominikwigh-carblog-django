c13bbab02f0e2f7d2eeaecd60c63bdae850e2f28
Dominikwigh <dominik-00@hotmail.com>
2023-02-10 12:19:20 +0000

    add django email backend

--- carblog/settings.py
+++ carblog/settings.py
@@ -139,3 +139,4 @@ LOGOUT_REDIRECT_URL = 'home'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
