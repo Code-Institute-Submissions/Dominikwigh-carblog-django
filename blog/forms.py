a81cf52b586a06d1fc4eabd72445c32475b6449d
Dominikwigh <dominik-00@hotmail.com>
2023-02-10 11:52:11 +0000

    add a contact form to forms.py

--- blog/forms.py
+++ blog/forms.py
@@ -32,3 +32,12 @@ class EditForm(forms.ModelForm):
             'slug': forms.TextInput(attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control'})
             }
+
+
+# Contact form 
+
+class ContactForm(forms.Form):
+    first_name = forms.CharField(max_length=50)
+    last_name = forms.CharField(max_length=50)
+    email_address = forms.EmailField(max_length=150)
+    message = forms.CharField(widget=forms.Textarea, max_length=2000)
