a4a47717fb6ff6d5b001663630cd5af2e4b4b204
Dominikwigh <dominik-00@hotmail.com>
2023-02-16 09:46:23 +0000

    create a edit view and a path

--- members/views.py
+++ members/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.urls import reverse_lazy
 
 
+# Registration
 class UserRegisterView(generic.CreateView):
     form_class = UserCreationForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('home')
+
+
+# Edit profile
+def UserEditView(request):
+    try:
+        profile = request.user.profile
+    except Profile.DoesNotExist:
+        profile = Profile(user=request.user)
+    if request.method == "POST":
+        form = ProfileForm(data=request.POST, files=request.FILES, instance=profile)
+        if form.is_valid():
+            form.save()
+            alert = True
+            return render(request, "edit_profile.html", {'alert': alert})
+    else:
+        form = ProfileForm(instance=profile)
+    return render(request, "edit_profile.html", {'form': form})
